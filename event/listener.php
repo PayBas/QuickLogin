5b1d164978824bcb705af22602f5552887bad7fc
PayBas <contact@paybas.com>
2015-01-12 01:32:43 +0100

    OAuth support (part 1)

--- event/listener.php
+++ event/listener.php
@@ -22,11 +22,17 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class listener implements EventSubscriberInterface
 {
+	/** @var \phpbb\auth\auth */
+	protected $auth_provider_collection;
+
 	/** @var \phpbb\config\config */
 	protected $config;
 
 	/** @var \phpbb\template\template */
 	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
 
 	/** @var string phpBB root path */
 	protected $root_path;
@@ -34,30 +40,49 @@ class listener implements EventSubscriberInterface
 	/** @var string PHP extension */
 	protected $phpEx;
 
-	public function __construct(\phpbb\config\config $config, \phpbb\template\template $template, $root_path, $phpEx)
+	public function __construct(\phpbb\auth\provider_collection $auth_provider_collection, \phpbb\config\config $config, \phpbb\template\template $template, \phpbb\user $user, $root_path, $phpEx)
 	{
-		$this->config    = $config;
-		$this->template  = $template;
-		$this->root_path = $root_path;
-		$this->phpEx     = $phpEx;
+		$this->auth_provider_collection = $auth_provider_collection;
+		$this->config                   = $config;
+		$this->template                 = $template;
+		$this->user                     = $user;
+		$this->root_path                = $root_path;
+		$this->phpEx                    = $phpEx;
 	}
 
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.page_header' => 'global_header',
+			'core.page_header_after' => 'quick_login',
 		);
 	}
 
 	/**
 	 */
-	public function global_header()
+	public function quick_login()
 	{
-		$tpl_vars = array(
-			'S_QUICK_LOGIN'       => true,
-			'U_SEND_PASSWORD_EXT' => ($this->config['email_enable']) ? append_sid("{$this->root_path}ucp.$this->phpEx", 'mode=sendpassword') : '',
-		);
+		if (!$this->user->data['is_registered'] && !$this->user->data['is_bot'])
+		{
+			$auth_provider = $this->auth_provider_collection->get_provider();
+			$auth_provider_data = $auth_provider->get_login_data();
 
-		$this->template->assign_vars($tpl_vars);
+			if ($auth_provider_data)
+			{
+				if (isset($auth_provider_data['BLOCK_VAR_NAME']) && ($auth_provider_data['BLOCK_VAR_NAME'] == 'oauth'))
+				{
+					foreach ($auth_provider_data['BLOCK_VARS'] as $block_vars)
+					{
+						// $this->template->assign_block_vars('ql_' . $auth_provider_data['BLOCK_VAR_NAME'], $block_vars); TODO: needs redirect fix for provider oauth.php
+					}
+				}
+			}
+
+			$tpl_vars = array(
+				'S_QUICK_LOGIN'       => true,
+				'U_SEND_PASSWORD_EXT' => ($this->config['email_enable']) ? append_sid("{$this->root_path}ucp.$this->phpEx", 'mode=sendpassword') : '',
+			);
+
+			$this->template->assign_vars($tpl_vars);
+		}
 	}
 }
